9c83a4dfd6f68295355aae2f5fc1cca9f889ea07
dododoSA <dododosa14@gmail.com>
2021-08-12 16:37:21 +0900

    fix: format

--- src/test/setupTests.ts
+++ src/test/setupTests.ts
@@ -1,4 +1,7 @@
+// テストなので以下の二つを抑止
+// eslint-disable-next-line import/no-extraneous-dependencies
 import Enzyme from 'enzyme';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
